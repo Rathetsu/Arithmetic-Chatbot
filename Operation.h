169607f171c994548ca7f7ee00d7ce447f8a4fc0
Rathetsu <ah.ezzat.muh@gmail.com>
2020-02-29 16:33:18 +0200

    Added 5 classes that inherit 'Operation'. Might add more later.

--- Operation.h
+++ Operation.h
@@ -14,7 +14,59 @@ private:
 
 public:
     Operation(int x, int y);
+    /*int Operation::_Add(int x, int y);
+    int Operation::_Subtract(int x, int y);
+    int Operation::_Multiply(int x, int y);
+    int Operation::_Divide(int x, int y);*/
+
+    virtual int calculate(int x, int y) = 0;
+
     ~Operation();
+};
+
+class _Add : Operation 
+{
+    int calculate(int x, int y)
+    {
+        int ans = x + y;
+        return ans;
+    }
+};
+
+class _Subtract : Operation 
+{
+    int calculate(int x, int y)
+    {
+        int ans = x - y;
+        return ans;
+    }
+};
+
+class _Difference : Operation 
+{
+    int calculate(int x, int y)
+    {
+        int ans = abs( x - y );
+        return ans;
+    }
+};
+
+class _Multiply : Operation 
+{
+    int calculate(int x, int y)
+    {
+        int ans = x * y;
+        return ans;
+    }
+};
+
+class _Divide : Operation 
+{
+    int calculate(int x, int y)
+    {
+        int ans = x / y;
+        return ans;
+    }
 };
 
 
